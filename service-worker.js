b599c790169e0b22fe40e1f0f840d9a88c348bc2
chikimmendes-cmyk <chikim.mendes@gmail.com>
2026-08-20 00:39:43 -0300

    Renova cache do PWA após correção da importação

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ftk-pwa-shell-v11620-v1';
+const CACHE='ftk-pwa-shell-v11620-v2';
 const SHELL=['./','./index.html','./setup.html','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
